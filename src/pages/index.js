1f61ec4ce5242285dab08ea79173533c2aef0d65
Jin Hao Chan <jinhao_@hotmail.com>
2021-04-15 07:14:28 -0700

    added sha to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,7 @@ const IndexPage = () => (
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
+      <div>Version %%VERSION%%</div>
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
