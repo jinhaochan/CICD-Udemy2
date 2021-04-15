ab0bb430d476d1616e8f1b3b36ec726dcfd56f4b
Jin Hao Chan <jinhao_@hotmail.com>
2021-04-15 05:39:25 -0700

    updated yml

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/user/CICD-Udemy2/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/user/CICD-Udemy2/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/home/user/CICD-Udemy2/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/home/user/CICD-Udemy2/src/pages/page-2.js")),
